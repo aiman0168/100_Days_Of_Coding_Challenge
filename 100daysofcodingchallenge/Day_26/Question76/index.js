9affe27b5a48f586878c0e86a0c03d254a8d2c5f
Umm e Aiman <160779331+aiman01689@users.noreply.github.com>
2024-03-31 02:12:06 +0500

    Add files via upload

---
+++ 100daysofcodingchallenge/Day_26/Question76/index.js
@@ -0,0 +1,7 @@
+//Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result. Show how you can call this function and log the result.
+function parameterandReturnvalue(num1, num2) {
+    return num1 + num2;
+}
+;
+//calling the function and logging the result.
+console.log(parameterandReturnvalue(1, 2));
---
+++ 100daysofcodingchallenge/Day_26/Question76/index.ts
@@ -0,0 +1,6 @@
+//Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result. Show how you can call this function and log the result.
+function parameterandReturnvalue (num1: number, num2: number): number {
+    return num1 + num2;
+};
+//calling the function and logging the result.
+console.log(parameterandReturnvalue(1,2));
