4a6d2249be397ef2e5070153341ce01d61658a85
Umm e Aiman <160779331+aiman01689@users.noreply.github.com>
2024-04-14 18:59:28 +0500

    Add files via upload

---
+++ 100daysofcodingchallenge/Day_40/Question120/index.ts
@@ -0,0 +1,6 @@
+//Use a for...of loop to iterate through an array of your favorite movies and log each one.
+const favoriteMovies: string[] = ["Damsel", "Ghosted", "12th Fail"];
+
+for (const movie of favoriteMovies){
+    console.log(movie)
+};
