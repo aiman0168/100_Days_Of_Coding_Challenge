2a81c24da1567b208beec81851a97d9401f9bd58
Umm e Aiman <160779331+aiman01689@users.noreply.github.com>
2024-04-18 02:26:45 +0500

    Add files via upload

---
+++ 100daysofcodingchallenge/Day_42/Question125/index.js
@@ -0,0 +1,9 @@
+//Modify a method in an object to use the this keyword to access another property in the same object.
+var sum = {
+    num1: 34,
+    num2: 45,
+    sumofNumbers: function () {
+        return this.num1 + this.num2;
+    },
+};
+console.log(sum.sumofNumbers());
