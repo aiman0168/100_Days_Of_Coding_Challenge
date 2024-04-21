89b933a7a53b9582439c48c77437132cd50207db
Umm e Aiman <160779331+aiman01689@users.noreply.github.com>
2024-04-22 00:17:23 +0500

    Add files via upload

---
+++ 100daysofcodingchallenge/Day_46/Question137/index.ts
@@ -0,0 +1,6 @@
+//Implement a try-catch block to handle potential errors in a block of code.
+try {
+    throw new Error ("Something went wrong")
+}catch (error){
+    console.log(error.message)
+};
