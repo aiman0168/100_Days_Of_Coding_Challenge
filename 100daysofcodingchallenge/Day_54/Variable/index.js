551f9f935e8ef55c88bdbd841788aff15ad02b8b
Umm e Aiman <160779331+aiman01689@users.noreply.github.com>
2024-04-27 06:59:05 +0500

    Add files via upload

---
+++ 100daysofcodingchallenge/Day_54/Variable/index.js
@@ -0,0 +1,12 @@
+let admin, name;
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+//Create a variable with the name of our planet.How would you name such a variable?
+let planet = "Earth";
+
+//Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentVisitor = "Sara";
